5e8d83b95d5252e7ac87d16034f82ffbdbd6ca74
Epsilon Alpha Mu <epsilon-alpha-mu@t60p.arenales.lan>
2020-05-08 12:27:15 -0300

    Edited msyql-db-dump.sh

--- msyql-db-dump.sh
+++ msyql-db-dump.sh
@@ -13,8 +13,8 @@ DB_00="xxxxxxxx"
 ## Directory where the backup is stored, WITHOUT a trailing slash
 DIR_DUMP_00="/var/db.dumps/xxxxxxxx"
 
-## Nombre del archivo, lo normal es que sea el mismo nombre de la BD
-DB_DUMP_00="tap_productio"
+## Name of the dump file, usually, the same name as the DB
+DB_DUMP_00="xxxxxxxx"
 
 $PRIORITY $MYSQL_DUMP -u$USER_00 -p$PASSWD_00 $DB_00 | $PRIORITY $GZIP > $DIR_DUMP_00/$DB_DUMP_00-$(date '+%Y%m%d').sql.gz
 
