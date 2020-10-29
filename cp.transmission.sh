d7c6d51c70caf24b3e74d3147f95f4f4cd8a0b42
eam-00 <64646896+eam-00@users.noreply.github.com>
2020-10-29 11:30:39 -0300

    Changed the DATE format

--- cp.transmission.sh
+++ cp.transmission.sh
@@ -5,7 +5,7 @@ case "$(pgrep $PROC | wc -w)" in
 
 0)  echo "Great: $PROC is not running, proceed with copy..."
     cd ~/.config/
-    cp -R transmission transmission.$(date '+%Y%m%d')
+    cp -R transmission transmission.$(date '+%Y-%m-%d')
     exit 1
     ;;
 1)  echo "Warning: $PROC is running, stopping this copy..."
