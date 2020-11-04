900194f28fdfb0e673d2f972d58751878cecf7cf
eam-00 <64646896+eam-00@users.noreply.github.com>
2020-11-04 00:35:16 -0300

    Added the cd to $MEDIA and cp

--- cp.transmission.sh
+++ cp.transmission.sh
@@ -6,6 +6,8 @@ case "$(pgrep $PROC | wc -w)" in
 0)  echo "Great: $PROC is not running, proceed with copy..."
     cd ~/.config/
     cp -R transmission transmission.$(date '+%Y-%m-%d')
+    cd /media/$ME/$BACKUP_PEN_DRIVE/Linux/hosts/viking/transmission/
+    cp -R * ~/.config/transmission/
     exit 1
     ;;
 1)  echo "Warning: $PROC is running, stopping this copy..."
