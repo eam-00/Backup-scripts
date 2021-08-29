780a5e6838ec6f50bd5dd4d60048e2f5d80c0477
eam-00 <64646896+eam-00@users.noreply.github.com>
2021-08-29 16:26:34 -0300

    New version

--- sync-files-to-usb.sh
+++ sync-files-to-usb.sh
@@ -1,42 +1,38 @@
 #!/usr/bin/env bash
 
 ## sync-files-to-usb.sh
-## Rsyncs some files to a USB drive, makes a mirror copy from the origin to the destination.
-## Before starting the rsync, checks that the USB drive is mounted.
-## from: Thinkpad T60p
+## Copy some files
+## from: Optiplex 755 - Debian Buster
 ## To: USB Backup (The Black Verbatim one)
 
-RSYNC=`which rsync`
-RSYNC_FLAGS='-avzr --delete --progress'
+RSYNC="/usr/bin/rsync"
+RSYNC_FLAGS="-avzr --delete --progress"
 ME=`/usr/bin/whoami`
-MKDIR='mkdir -p'
-BACKUP_PEN_DRIVE='Backup'
+MKDIR="mkdir -p"
 
-$MKDIR /media/$ME/$BACKUP_PEN_DRIVE/Linux/hosts/T60p
+BACKUP_PEN_DRIVE="Backup"
 
 if [ -d "/media/$ME/$BACKUP_PEN_DRIVE/" -a -w "/media/$ME/$BACKUP_PEN_DRIVE/" ]
 then
 
-    echo "OK... Backup Starting..."
+    echo "Todo OK... Backupeando..."
 
-DEST="/media/$ME/$BACKUP_PEN_DRIVE/Linux/hosts/T60p"
+DEST="/media/$ME/$BACKUP_PEN_DRIVE/Linux/hosts/Optiplex-755-usff"
 
 $MKDIR $DEST/etc/
 $MKDIR $DEST/etc/ssh/
 $MKDIR $DEST/etc/apt/
 $MKDIR $DEST/etc/lightdm/
+$MKDIR $DEST/etc/ansible/
 
-$RSYNC $RSYNC_FLAGS /etc/hosts			$DEST/etc/
-$RSYNC $RSYNC_FLAGS /etc/fstab			$DEST/etc/
-$RSYNC $RSYNC_FLAGS /etc/sysctl.conf		$DEST/etc/
-$RSYNC $RSYNC_FLAGS /etc/ssh/sshd_config	$DEST/etc/ssh/
-$RSYNC $RSYNC_FLAGS /etc/ssh/banner		$DEST/etc/ssh/
-
-cd /etc/apt/
-$RSYNC $RSYNC_FLAGS * $DEST/etc/apt/
-
-cd /etc/lightdm/
-$RSYNC $RSYNC_FLAGS * $DEST/etc/lightdm/
+head -n 60 /etc/hosts > $DEST/etc/hosts.txt
+$RSYNC /etc/fstab $DEST/etc/
+$RSYNC /etc/sysctl.conf $DEST/etc/
+$RSYNC /etc/ssh/sshd_config $DEST/etc/ssh/
+$RSYNC /etc/ssh/banner $DEST/etc/ssh/
+$RSYNC $RSYNC_FLAGS /etc/apt $DEST/etc/apt/
+$RSYNC $RSYNC_FLAGS /etc/lightdm/ $DEST/etc/lightdm/
+$RSYNC $RSYNC_FLAGS /etc/ansible/ $DEST/etc/ansible/
 
 $MKDIR $DEST/home/$ME/
 $MKDIR $DEST/home/$ME/Emacs/
@@ -45,22 +41,31 @@ $MKDIR $DEST/home/$ME/.ssh/
 $MKDIR $DEST/home/$ME/Local
 $MKDIR $DEST/home/$ME/.config/
 $MKDIR $DEST/home/$ME/.config/vlc/
+$MKDIR $DEST/home/$ME/.config/i3/
 
-$RSYNC $RSYNC_FLAGS ~/.emacs			$DEST/home/$ME/
-$RSYNC $RSYNC_FLAGS ~/.bashrc			$DEST/home/$ME/
-$RSYNC $RSYNC_FLAGS ~/.Xresources		$DEST/home/$ME/
-$RSYNC $RSYNC_FLAGS ~/.links2/bookmarks.html	$DEST/home/$ME/.links2/
+$RSYNC $RSYNC_FLAGS ~/todo.txt $DEST/home/$ME/
+$RSYNC $RSYNC_FLAGS ~/.emacs $DEST/home/$ME/
+$RSYNC $RSYNC_FLAGS ~/.bashrc $DEST/home/$ME/
+$RSYNC $RSYNC_FLAGS ~/.bash_aliases $DEST/home/$ME/
+$RSYNC $RSYNC_FLAGS ~/.Xresources $DEST/home/$ME/
+$RSYNC $RSYNC_FLAGS ~/.links2/bookmarks.html $DEST/home/$ME/.links2/
 
-cd /home/$ME/.config/vlc/
+cd ~/.config/vlc/
 $RSYNC $RSYNC_FLAGS * $DEST/home/$ME/.config/vlc/
 
-cd /home/$ME/.ssh
+cd ~/.config/i3/
+$RSYNC $RSYNC_FLAGS * $DEST/home/$ME/.config/i3/
+
+cd ~/.config/i3status/
+$RSYNC $RSYNC_FLAGS * $DEST/home/$ME/.config/i3status/
+
+cd ~/.ssh
 $RSYNC $RSYNC_FLAGS * $DEST/home/$ME/.ssh/
 
-cd /home/$ME/Local
-$RSYNC $RSYNC_FLAGS --exclude 'DSCN8324.00.jpg' --exclude 'DSCN8325.00.jpg' * $DEST/home/$ME/Local/
+cd ~/Local
+$RSYNC $RSYNC_FLAGS --exclude 'DSCN8324.00.jpg' --exclude 'DSCN8325.00.jpg' --exclude 'Stuffs' * $DEST/home/$ME/Local/
 
-cd /home/$ME/Emacs/
+cd ~/Emacs/
 $RSYNC $RSYNC_FLAGS --exclude 'Backups/*' * $DEST/home/$ME/Emacs/
 
 fi
