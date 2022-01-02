00afd2c9803618ed4b5514c79302ce391cae3b4b
eam-00 <64646896+eam-00@users.noreply.github.com>
2022-01-01 21:49:11 -0300

    Replaced TAR with MY_TAR

--- tar-backups.sh
+++ tar-backups.sh
@@ -9,35 +9,35 @@ HOST=`/bin/hostname`
 ## No trailing slash!
 DEST="/home/xxxxxxxxx"
 MV="/bin/mv"
-TAR="/bin/tar cip"
+MY_TAR="/bin/tar cip"
 
  if [ -f "$DEST/"${HOST}"-etc.tar" ]; then
     $MV $DEST/"${HOST}"-etc.tar $DEST/"${HOST}"-etc.tar.OLD
  fi
-$TAR /etc/ -f $DEST/$HOST-etc.tar
+$MY_TAR /etc/ -f $DEST/$HOST-etc.tar
 
 
  if [ -f "$DEST/"${HOST}"-root-admin.tar" ]; then
     $MV $DEST/"${HOST}"-root-admin.tar $DEST/"${HOST}"-root-admin.tar.OLD
  fi
-$TAR /root/admin/ -f $DEST/$HOST-root-admin.tar
+$MY_TAR /root/admin/ -f $DEST/$HOST-root-admin.tar
 
 
  if [ -f "$DEST/"${HOST}"-usr-local-etc-scripts.tar" ]; then
     $MV $DEST/"${HOST}"-usr-local-etc-scripts.tar $DEST/"${HOST}"-usr-local-etc-scripts.tar.OLD
  fi
-$TAR /usr/local/etc/scripts/ -f $DEST/$HOST-usr-local-etc-scripts.tar
+$MY_TAR /usr/local/etc/scripts/ -f $DEST/$HOST-usr-local-etc-scripts.tar
 
 
  if [ -f "$DEST/"${HOST}"-var-spool-cron-crontabs.tar" ]; then
     $MV $DEST/"${HOST}"-var-spool-cron-crontabs.tar $DEST/"${HOST}"-var-spool-cron-crontabs.tar.OLD
  fi
-$TAR /var/spool/cron/crontabs/ -f $DEST/$HOST-var-spool-cron-crontabs.tar
+$MY_TAR /var/spool/cron/crontabs/ -f $DEST/$HOST-var-spool-cron-crontabs.tar
 
 
  if [ -f "$DEST/"${HOST}"-home-deploy-staging-shared-config.tar" ]; then
     $MV $DEST/"${HOST}"-home-deploy-staging-shared-config.tar $DEST/"${HOST}"-home-deploy-staging-shared-config.tar.OLD
  fi
-$TAR /home/deploy/staging/shared/config/ -f $DEST/$HOST-home-deploy-staging-shared-config.tar
+$MY_TAR /home/deploy/staging/shared/config/ -f $DEST/$HOST-home-deploy-staging-shared-config.tar
 
 ## EoF ##
