cdb9194159fa782f092fbce08e3f426919bc4017
eam-00 <64646896+eam-00@users.noreply.github.com>
2022-01-16 17:46:26 -0300

    Added opt_rsync_dryrun

--- sync-dropbox.sh
+++ sync-dropbox.sh
@@ -9,6 +9,7 @@ echo
 echo
 
 cmd_rsync=$(which rsync)
+opt_rsync_dryrun="-e ssh --progress --delete -navzr"
 
 cd ~/Dropbox
 ${cmd_rsync} -e ssh --progress --delete -avzr * ${server_name}:~/Dropbox/
