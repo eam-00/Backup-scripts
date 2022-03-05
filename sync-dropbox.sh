01ec18853e73bdb2f632cde451ead0d2b034cdf9
eam-00 <64646896+eam-00@users.noreply.github.com>
2022-03-05 13:08:56 -0300

    Added X60

--- sync-dropbox.sh
+++ sync-dropbox.sh
@@ -12,6 +12,10 @@ cmd_rsync=$(which rsync)
 opt_rsync_dryrun="-e ssh --progress --delete -navzr"
 opt_rsync_real="-e ssh --progress --delete -avzr"
 
+## X60
+cd ~/Local/
+${server_name}:~/Local/
+
 cd ~/Dropbox
 ${cmd_rsync} -e ssh --progress --delete -avzr * ${server_name}:~/Dropbox/
 
