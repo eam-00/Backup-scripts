8122ea72c398be97f799129e0076fa0a56103eac
eam-00 <64646896+eam-00@users.noreply.github.com>
2022-03-05 17:29:24 -0300

    Added the if eval for X60

--- sync-dropbox.sh
+++ sync-dropbox.sh
@@ -13,11 +13,17 @@ opt_rsync_dryrun="-e ssh --progress --delete -navzr"
 opt_rsync_real="-e ssh --progress --delete -avzr"
 
 ## X60
+if [ ${server_name} == X60 ]; then
+
 cd ~/Local/
 ${cmd_rsync} ${opt_rsync_dryrun} ${server_name}:~/Local/
 
+else
+
 cd ~/Dropbox
 ${cmd_rsync} -e ssh --progress --delete -avzr * ${server_name}:~/Dropbox/
+
+fi
 
 ## EoF ##
 
