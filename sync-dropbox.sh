681292ff0f182e8d2fba0bb1a00c545cee7bf03c
eam-00 <64646896+eam-00@users.noreply.github.com>
2022-03-05 12:45:39 -0300

    Added opt_rsync_real

--- sync-dropbox.sh
+++ sync-dropbox.sh
@@ -10,6 +10,7 @@ echo
 
 cmd_rsync=$(which rsync)
 opt_rsync_dryrun="-e ssh --progress --delete -navzr"
+opt_rsync_real="-e ssh --progress --delete -avzr"
 
 cd ~/Dropbox
 ${cmd_rsync} -e ssh --progress --delete -avzr * ${server_name}:~/Dropbox/
