788dfb6b2d14243c98202298aa01bd3c3d1956f4
eam-00 <64646896+eam-00@users.noreply.github.com>
2022-03-06 16:02:06 -0300

    Added Host: ${server_name} for each Rsync

--- sync-dropbox.sh
+++ sync-dropbox.sh
@@ -32,16 +32,31 @@ cd ~/Downloads
 ${cmd_rsync} ${opt_rsync_dryrun} --exclude 'Orig.dloads' * ${server_name}:~/Downloads/
 }
 
+## -----------------------------------------------
 
 if [ ${server_name} == x60 ]; then
+echo -e
+echo -e  Host: ${server_name}
+echo -e
+echo -e  Directory: ~/Local
+echo -e
  rsync_local
+echo -e
+echo -e  Directory: ~/Dropbox
+echo -e
  rsync_dropbox
 
 elif [ ${server_name} == t410 ]; then
+echo -e
+echo -e  Host: ${server_name}
+echo -e
  rsync_local
  rsync_dropbox
 
 elif [ ${server_name} == t60p ]; then
+echo -e
+echo -e  Host: ${server_name}
+echo -e
  rsync_local
  rsync_dropbox
  rsync_music
@@ -50,8 +65,12 @@ elif [ ${server_name} == t60p ]; then
 else
 
 cd ~/Dropbox
-echo -e "Proceed with Dropbox Rsync... "
+echo -e "Rsync only Dropbox..."
+echo -e
+echo -e  Host: ${server_name}
+echo -e
  rsync_dropbox
+
 fi
 
-## EOF ##
+## EoF ##
