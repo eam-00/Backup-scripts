042bac9100e931e6f94fb81e6ea203098580f6a1
eam-00 <64646896+eam-00@users.noreply.github.com>
2022-11-17 20:38:31 -0300

    Added cmd_whoami

--- cp.transmission.sh
+++ cp.transmission.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 PROC='transmission'
-ME=`/usr/bin/whoami`
+cmd_whoami=$(which whoami)
 BACKUP_PEN_DRIVE='Backup'
 
 case "$(pgrep $PROC | wc -w)" in
@@ -9,7 +9,7 @@ case "$(pgrep $PROC | wc -w)" in
 0)  echo "Great: $PROC is not running, proceed with copy..."
     cd ~/.config/
     cp -R transmission transmission.$(date '+%Y-%m-%d')
-    cd /media/$ME/$BACKUP_PEN_DRIVE/Linux/hosts/viking/transmission/
+    cd /media/${cmd_whoami}/$BACKUP_PEN_DRIVE/Linux/hosts/viking/transmission/
     cp -f -R * ~/.config/transmission/
     exit 1
     ;;
